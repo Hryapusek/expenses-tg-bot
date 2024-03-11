c147dd14bd5ff2c96635f3bf5ad1bd172e7f8c3a
Abraamyan Alexander <hryapusek@gmail.com>
2024-03-12 02:44:38 +0300

    database files were created

---
+++ docker_database/initdb.sql
@@ -0,0 +1,39 @@
+CREATE TABLE person (
+  id BIGINT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  cathegory_ids BIGINT[],
+  balance BIGINT NOT NULL
+);
+
+CREATE TABLE cathegory_type (
+  id BIGSERIAL PRIMARY KEY,
+  type_name TEXT NOT NULL
+);
+
+INSERT INTO cathegory_type(type_name) VALUES ('expense'), ('income');
+
+CREATE TABLE operation_type (
+  id BIGSERIAL PRIMARY KEY,
+  type_name TEXT NOT NULL
+);
+
+INSERT INTO operation_type(type_name) VALUES ('expense'), ('income');
+
+CREATE TABLE cathegory (
+  id BIGSERIAL PRIMARY KEY,
+  person_id BIGINT REFERENCES person(id),
+  cathegory_type_id BIGINT REFERENCES cathegory_type(id),
+  name TEXT NOT NULL,
+  money_limit BIGINT NOT NULL DEFAULT 0,
+  current_money BIGINT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE operation (
+  id BIGSERIAL PRIMARY KEY,
+  date TIMESTAMP NOT NULL DEFAULT now(),
+  operation_type_id BIGINT REFERENCES operation_type(id),
+  person_id BIGINT REFERENCES person(id),
+  cathegory_id BIGINT REFERENCES cathegory(id),
+  money_amount BIGINT NOT NULL,
+  commentary TEXT
+);
