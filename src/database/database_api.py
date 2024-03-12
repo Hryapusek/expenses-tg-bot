1499be8ab247e142f5f7a952a160dc1c8d94ee82
Nikiforova Ekaterina <nikiforovaa.ekaterinaa@gmail.com>
2024-03-12 19:02:00 +0300

    database api beginning

---
+++ src/database/database_api.py
@@ -0,0 +1,19 @@
+from singleton_decorator import singleton
+
+@singleton
+class DatabaseApi:
+    def get_person_by_id(id):
+        pass
+
+    def add_person(person):
+        pass
+    
+    def add_cathegory(cathegory):
+        pass
+
+    def remove_cathegory(cathegory):
+        pass
+    
+    def add_operation(operation):
+        pass
+    
