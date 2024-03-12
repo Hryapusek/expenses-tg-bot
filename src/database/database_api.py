13d989f23d3a901b47b6d63b48fbe31499da9dc1
Abraamyan Alexander <samvelcc25@outlook.com>
2024-03-12 20:32:06 +0300

    get_person_by_id raw version

--- src/database/database_api.py
+++ src/database/database_api.py
@@ -1,9 +1,26 @@
 from singleton_decorator import singleton
+from .connection import DatabaseConnection
+from .types.person import Person
+import psycopg2
+
 
 @singleton
 class DatabaseApi:
     def get_person_by_id(id):
-        pass
+        """
+        Returns
+        -------
+            Person: person object from table
+
+        Raise
+        -----
+            - ProgrammingError if no person found
+            - OperationalError if connection establishing failed
+        """
+        conn = DatabaseConnection.connection()
+        with conn.cursor() as cursor:
+            cursor.execute('SELECT * FROM person WHERE id = %s' % id)
+            return Person.fromTuple(cursor.fetchone())
 
     def add_person(person):
         pass
