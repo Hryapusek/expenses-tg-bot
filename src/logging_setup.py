6f4714f2e94ccc1e56df747efaa34307b99f274b
Nikiforova Ekaterina <nikiforovaa.ekaterinaa@gmail.com>
2024-03-12 19:01:46 +0300

    logging added

---
+++ src/logging_setup.py
@@ -0,0 +1,14 @@
+import logging
+
+def logging_setup():
+    root_logger = logging.getLogger()
+    logger_level = logging.DEBUG if __debug__ else logging.INFO
+    logger_format = "%(name)s %(asctime)s %(levelname)s %(message)s"
+    logging.basicConfig(
+        level=logger_level,
+        format=logger_format
+    )
+    file_handler = logging.FileHandler("general.log", mode='w')
+    formatter = logging.Formatter(logger_format)
+    file_handler.setFormatter(formatter)
+    root_logger.addHandler(file_handler)
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from logging_setup import logging_setup
+import logging
+
+def main():
+    logging_setup()
+    logging.info("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
