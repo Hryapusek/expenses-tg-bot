98b7741bbab6afdbeb3ba848ea81e1f5d209c084
Abraamyan Alexander <samvelcc25@outlook.com>
2024-03-13 21:52:38 +0300

    rename

--- src/database/types/cathegory.py
+++ src/database/types/cathegory.py
@@ -1,8 +1,8 @@
 class Cathegory:
-    def __init__(self, id, person_id, cathegory_type, name, money_limit, current_money) -> None:
+    def __init__(self, id, person_id, cathegory_type_id, name, money_limit, current_money) -> None:
         self.id = id
         self.person_id = person_id
-        self.cathegory_type = cathegory_type
+        self.cathegory_type_id = cathegory_type_id
         self.name = name
         self.money_limit = money_limit
         self.current_money = current_money
@@ -12,7 +12,7 @@ class Cathegory:
         cathegory = Cathegory (
             id = data[0],
             person_id = data[1],
-            cathegory_type = data[2],
+            cathegory_type_id = data[2],
             name = data[3],
             money_limit = data[4],
             current_money = data[5],
