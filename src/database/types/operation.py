f153b726239150eadff733a3debdea3363071193
Abraamyan Alexander <hryapusek@gmail.com>
2024-03-16 20:36:50 +0300

    __eq__ was added

--- src/database/types/operation.py
+++ src/database/types/operation.py
@@ -1,9 +1,17 @@
+from __future__ import annotations
 import datetime
 
 class Operation:
-    def __init__(self, id = 0, date = datetime.datetime.now(), 
-                 operation_type_id = 0, person_id = 0, 
-                 cathegory_id = 0, money_amout = 0, comment = ''):
+    def __init__(
+        self,
+        id=0,
+        date=datetime.datetime.now(),
+        operation_type_id=0,
+        person_id=0,
+        cathegory_id=0,
+        money_amout=0,
+        comment="",
+    ):
         self.id = id
         self.date = date
         self.operation_type_id = operation_type_id
@@ -15,11 +23,22 @@ class Operation:
     @staticmethod
     def fromTuple(data):
         operation = Operation(
-            id = data[0],
-            date = data[1],
-            operation_type_id = data[2],
-            person_id = data[3],
-            cathegory_id = data[4],
-            money_amout = data[5],
-            comment = data[6],
+            id=data[0],
+            date=data[1],
+            operation_type_id=data[2],
+            person_id=data[3],
+            cathegory_id=data[4],
+            money_amout=data[5],
+            comment=data[6],
+        )
+        return operation
+
+    def __eq__(self, other: Operation) -> bool:
+        return (
+            self.id == other.id
+            and self.date == other.date
+            and self.operation_type_id == other.operation_type_id
+            and self.cathegory_id == other.cathegory_id
+            and self.money_amout == other.money_amout
+            and self.comment == other.comment
         )
