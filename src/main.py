3d43e088d55e471011700cfe90fbf17b03f97129
Gennady <vip.vb1980@mail.ru>
2024-03-23 23:27:05 +0300

    bot polling added with some initialization

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from logging_setup import logging_setup
+from configreader import ConfigReader
 import logging
+import telebot
+from telebot import types
+from messageprocessing.router.message_router import MessageRouter
+from messageprocessing.botstate import BotState
 
 def main():
     logging_setup()
     logging.info("Hello world!")
+    bot = telebot.TeleBot(ConfigReader().bot_token)
+    BotState(bot)
+    bot.register_message_handler(MessageRouter().process_message, func = lambda x: True)
+    bot.polling()
 
 
 if __name__ == "__main__":
