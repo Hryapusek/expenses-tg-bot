f167c1a39c882b4c32133725cc75c1578ce6f6d9
Abraamyan <samvelcc25@outlook.com>
2024-04-05 01:28:04 +0300

    function was separated

--- src/messageprocessing/handlers/cathegorieshandler/manage_cathegories_hndl.py
+++ src/messageprocessing/handlers/cathegorieshandler/manage_cathegories_hndl.py
@@ -1,11 +1,13 @@
 from messageprocessing.handlers.base_hndl import *
 from telebot.types import Message, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 
+from messageprocessing.handlers.cathegorieshandler.change_cathegory_handler import ChangeCathegoryHandler
 from messageprocessing.handlers.cathegorieshandler.create_cathegory_handler import CreateCathegoryHandler
 from ...botstate import BotState
 from database.api import DatabaseApi
 from database.types.person import Person
 from database.types.cathegory import Cathegory
+from .utils import cathegories_to_string
 
 
 class ManageCathegoriesHandler(BaseHandler):
@@ -37,54 +39,40 @@ class CathegoriesMainMenuHandler(ReusableHandler):
 
     def __init__(self) -> None:
         self.person: Person = None
-        self.cathegories: list[Cathegory] = []
+        self.income_cathegories: list[Cathegory] = []
+        self.expense_cathegories: list[Cathegory] = []
 
     def handle_message(self, message: Message):
         if not message.text:
             return self
         if message.text == __class__.CREATE_CATHEGORY_BUTTON_NAME:
             return CreateCathegoryHandler.switch_to_this_handler(message, self)
+        elif message.text == __class__.CHANGE_CATHEGORY_BUTTON_NAME:
+            return ChangeCathegoryHandler.switch_to_this_handler(message, self)
         return self
-        pass
 
     @staticmethod
     def switch_to_this_handler(message: Message):
         handler = CathegoriesMainMenuHandler()
         handler.load_data_from_database(message)
         result = ""
-        cathegories_str = handler.cathegories_to_string()
+        cathegories_str = cathegories_to_string(handler.income_cathegories, handler.expense_cathegories)
         result += cathegories_str + __class__.CHOOSE_OPTION_MESSAGE
         BotState().bot.send_message(message.chat.id, result, reply_markup=__class__.MARKUP)
         return handler
     
     def switch_to_existing_handler(self, message: Message):
         result = ""
-        cathegories_str = self.cathegories_to_string()
+        cathegories_str = cathegories_to_string(self.income_cathegories, self.expense_cathegories)
         result += cathegories_str + __class__.CHOOSE_OPTION_MESSAGE
         BotState().bot.send_message(message.chat.id, result, reply_markup=__class__.MARKUP)
         return self
 
     def load_data_from_database(self, message: Message):
         self.cathegories = DatabaseApi().get_person_all_cathegories_by_id(message.from_user.id)
-
-    def cathegories_to_string(self) -> str:
-        if len(self.cathegories) == 0:
-            return "Нам не удалось найти ни одной вашей категории."
-        result = ""
-        line_format = ("{}. {}\n"
-                       "\tБаланс: {:,}\n"
-                       "\tЛимит: {:,}")
-        income_cathegories = filter(lambda x: x.cathegory_type_id == DatabaseApi().get_income_cathegory_type_id(), 
+        self.income_cathegories = filter(lambda x: x.cathegory_type_id == DatabaseApi().get_income_cathegory_type_id(), 
                                     self.cathegories)
-        expense_cathegories = filter(lambda x: x.cathegory_type_id == DatabaseApi().get_expense_cathegory_type_id(), 
+        self.income_cathegories = list(self.income_cathegories)
+        self.expense_cathegories = filter(lambda x: x.cathegory_type_id == DatabaseApi().get_expense_cathegory_type_id(), 
                                     self.cathegories)
-        start_number = 1
-        for cathegory_type_name, cathegories in (("Доходные категории", income_cathegories),
-                                               ("Расходные категории", expense_cathegories)):
-            result += cathegory_type_name + ":\n"
-            for seq_no, cathegory in enumerate(cathegories, start_number):
-                line = line_format.format(seq_no, cathegory.name, cathegory.current_money, cathegory.money_limit)
-                result += line + '\n'
-                start_number = seq_no
-            result += '\n\n'
-        return result
+        self.expense_cathegories = list(self.expense_cathegories)
