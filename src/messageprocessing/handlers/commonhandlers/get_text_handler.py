34847d7730aeec855abef1acba005e0df7deb5c9
Abraamyan <samvelcc25@outlook.com>
2024-04-08 17:25:44 +0300

    cancel button added

--- src/messageprocessing/handlers/commonhandlers/get_text_handler.py
+++ src/messageprocessing/handlers/commonhandlers/get_text_handler.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 from ..base_inner_hndl import ReturningResultHandler, BaseHandler, ReusableHandler
-from telebot.types import Message, ReplyKeyboardRemove
+from telebot.types import Message, ReplyKeyboardMarkup
 from botstate import BotState
 
 
-class GetNameHandler(ReturningResultHandler):
+class GetTextHandler(ReturningResultHandler):
+
+    CANCEL_NAME = "Отмена"
 
     def __init__(self, outter_handler: ReusableHandler, asking_message: str, 
-                 markup = ReplyKeyboardRemove(), pred = lambda x: (True, "")) -> None:
+                 markup = ReplyKeyboardMarkup(), pred = lambda x: (True, "")) -> None:
         super().__init__(outter_handler)
         self.asking_message = asking_message
         self.markup = markup
@@ -16,6 +18,9 @@ class GetNameHandler(ReturningResultHandler):
     def handle_message(self, message: Message) -> BaseHandler:
         if not message.text:
             return self
+        if message.text == __class__.CANCEL_NAME:
+            self.outter_handler.return_result = None
+            return self.outter_handler.switch_to_existing_handler(message)
         result, err = self.pred(message.text)
         if not result:
             BotState().bot.send_message(message.chat.id, err, reply_markup=self.markup)
@@ -26,7 +31,7 @@ class GetNameHandler(ReturningResultHandler):
 
     @staticmethod
     def switch_to_this_handler(message: Message, outter_handler: ReusableHandler, 
-                               asking_message: str, markup = ReplyKeyboardRemove(), 
-                               pred = lambda x: (True, "")) -> GetNameHandler:
+                               asking_message: str, markup = ReplyKeyboardMarkup(), 
+                               pred = lambda x: (True, "")) -> GetTextHandler:
         BotState().bot.send_message(message.chat.id, asking_message, reply_markup=markup)
-        return GetNameHandler(outter_handler, asking_message, markup, pred)
+        return GetTextHandler(outter_handler, asking_message, markup, pred)
