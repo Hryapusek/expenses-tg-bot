03f4782bda02b653eac3d7c6511078f79872fc26
Abraamyan <samvelcc25@outlook.com>
2024-04-10 09:36:02 +0300

    CathegoriesMainMenuHandler changed to new style

--- src/messageprocessing/handlers/cathegorieshandler/manage_cathegories_hndl.py
+++ src/messageprocessing/handlers/cathegorieshandler/manage_cathegories_hndl.py
@@ -1,72 +1,87 @@
 from messageprocessing.handlers.base_hndl import *
-from telebot.types import Message, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import Message
 
 from messageprocessing.handlers.cathegorieshandler.change_cathegory_handler import ChangeCathegoryHandler
 from messageprocessing.handlers.cathegorieshandler.create_cathegory_handler import CreateCathegoryHandler
-from ...botstate import BotState
+from messageprocessing.handlers.commonhandlers.choose_option_handler import ChooseOptionHandler
 from database.api import DatabaseApi
-from database.types.person import Person
 from database.types.cathegory import Cathegory
 from .utils import cathegories_to_string
-
-
-class ManageCathegoriesHandler(BaseHandler):
-
-    def handle_message(self, message: Message):
-        return self
-
-    @staticmethod
-    def switch_to_this_handler(message: Message):
-        handler = CathegoriesMainMenuHandler.switch_to_this_handler(
-            message
-        )
-        return handler
 
 
 class CathegoriesMainMenuHandler(ReusableHandler):
     CHOOSE_OPTION_MESSAGE = "\n\nВыберите действие"
 
-    MARKUP = ReplyKeyboardMarkup()
-    CREATE_CATHEGORY_BUTTON_NAME = "Создать категорию"
-    CHANGE_CATHEGORY_BUTTON_NAME = "Изменить категорию"
-    DELETE_CATHEGORY_BUTTON_NAME = "Удалить категорию"
-    FINISH_BUTTON_NAME = "Завершить категорию"
+    CREATE_CATHEGORY_OPTION_NAME = "Создать категорию"
+    CHANGE_CATHEGORY_OPTION_NAME = "Изменить категорию"
+    DELETE_CATHEGORY_OPTION_NAME = "Удалить категорию"
+    FINISH_OPTION_NAME = "Завершить"
 
-    MARKUP.add(CREATE_CATHEGORY_BUTTON_NAME)
-    MARKUP.add(CHANGE_CATHEGORY_BUTTON_NAME)
-    MARKUP.add(DELETE_CATHEGORY_BUTTON_NAME)
-    MARKUP.add(FINISH_BUTTON_NAME)
+
+    OPTIONS = [
+        CREATE_CATHEGORY_OPTION_NAME,
+        CHANGE_CATHEGORY_OPTION_NAME,
+        DELETE_CATHEGORY_OPTION_NAME,
+        FINISH_OPTION_NAME,
+    ]
+
+    CHOOSING_OPTION_STATE = 0
+    OTHER_STATE = 1
 
     def __init__(self) -> None:
-        self.person: Person = None
         self.income_cathegories: list[Cathegory] = []
         self.expense_cathegories: list[Cathegory] = []
+        self.state = __class__.CHOOSING_OPTION_STATE
 
     def handle_message(self, message: Message):
-        if not message.text:
-            return self
-        if message.text == __class__.CREATE_CATHEGORY_BUTTON_NAME:
-            return CreateCathegoryHandler.switch_to_this_handler(message, self)
-        elif message.text == __class__.CHANGE_CATHEGORY_BUTTON_NAME:
-            return ChangeCathegoryHandler.switch_to_this_handler(message, self)
         return self
 
     @staticmethod
     def switch_to_this_handler(message: Message):
-        handler = CathegoriesMainMenuHandler()
-        handler.load_data_from_database(message)
-        result = ""
-        cathegories_str = cathegories_to_string(handler.income_cathegories, handler.expense_cathegories)
-        result += cathegories_str + __class__.CHOOSE_OPTION_MESSAGE
-        BotState().bot.send_message(message.chat.id, result, reply_markup=__class__.MARKUP)
-        return handler
+        main_handler = CathegoriesMainMenuHandler()
+        main_handler.load_data_from_database(message)
+        asking_message = ""
+        cathegories_str = cathegories_to_string(main_handler.income_cathegories, main_handler.expense_cathegories)
+        asking_message += cathegories_str + __class__.CHOOSE_OPTION_MESSAGE
+        return ChooseOptionHandler.switch_to_this_handler(message, main_handler, 
+                                                          asking_message, __class__.OPTIONS,
+                                                          add_cancel=False)
     
     def switch_to_existing_handler(self, message: Message):
-        result = ""
+        if self.state == __class__.OTHER_STATE:
+            return self.other_sh(message)
+        elif self.state == __class__.CHOOSING_OPTION_STATE:
+            return self.choosing_option_sh(message)
+    
+    def other_sh(self, message: Message):
+        asking_message = ""
         cathegories_str = cathegories_to_string(self.income_cathegories, self.expense_cathegories)
-        result += cathegories_str + __class__.CHOOSE_OPTION_MESSAGE
-        BotState().bot.send_message(message.chat.id, result, reply_markup=__class__.MARKUP)
-        return self
+        asking_message += cathegories_str + __class__.CHOOSE_OPTION_MESSAGE
+        prev_state = self.state
+        self.state = __class__.CHOOSING_OPTION_STATE
+        try:
+            return ChooseOptionHandler.switch_to_this_handler(message, self, 
+                                                                asking_message, __class__.OPTIONS,
+                                                                add_cancel=False)
+        except:
+            self.state = prev_state
+            raise
+    
+    def choosing_option_sh(self, message: Message):
+        if not message.text:
+            return self
+        try:
+            prev_state = self.state
+            if message.text == __class__.CREATE_CATHEGORY_OPTION_NAME:
+                self.state = __class__.OTHER_STATE
+                return CreateCathegoryHandler.switch_to_this_handler(message, self)
+            elif message.text == __class__.CHANGE_CATHEGORY_OPTION_NAME:
+                self.state = __class__.OTHER_STATE
+                return ChangeCathegoryHandler.switch_to_this_handler(message, self)
+        except:
+            self.state = prev_state
+            raise
+        assert False, "This can not be reached. Incorrect option handling?"
 
     def load_data_from_database(self, message: Message):
         self.cathegories = DatabaseApi().get_person_all_cathegories_by_id(message.from_user.id)
