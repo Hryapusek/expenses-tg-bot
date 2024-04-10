13eb01e944378fe950cc7cb55d3741e1774ed6c1
Nikiforova <nikiforovaa.ekaterinaa@gmail.com>
2024-04-10 23:09:46 +0300

    new style was applied for this handler

--- src/messageprocessing/handlers/cathegorieshandler/create_cathegory_handler.py
+++ src/messageprocessing/handlers/cathegorieshandler/create_cathegory_handler.py
@@ -1,178 +1,183 @@
+from enum import Enum
 import logging
 from messageprocessing.handlers.base_hndl import BaseHandler, ReusableHandler
-from telebot.types import Message, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import (
+    Message,
+    ReplyKeyboardRemove,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+)
+
+from messageprocessing.handlers.cathegorieshandler.commonhandlers.choose_cathegory_type_handler import (
+    ChooseCathegoryTypeHandler,
+)
+from messageprocessing.handlers.commonhandlers.get_number_handler import GetNumberHandler
+from messageprocessing.handlers.commonhandlers.get_text_handler import GetTextHandler
 from ...botstate import BotState
-from ..base_inner_hndl import BaseInnerHandler
+from ..base_inner_hndl import BaseInnerHandler, ReturningResultHandler
 from database.api import DatabaseApi
 from database.types.person import Person
 from database.types.cathegory import Cathegory
 
 # TODO: add cancel button everywhere
 
-class CreateCathegoryHandler(BaseInnerHandler):
 
-    def handle_message(self, message: Message):
-        pass # Nothing to do here
+class CreateCathegoryHandler(ReturningResultHandler, ReusableHandler):
 
-    @staticmethod
-    def switch_to_this_handler(message: Message, outter_handler) -> BaseHandler:
-        cathegory = Cathegory()
-        cathegory.person_id = message.from_user.id
-        return _ChooseCathegoryTypeHandler.switch_to_this_handler(message, outter_handler, cathegory)
+    class State(Enum):
+        WAITING_FOR_TYPE = 1
+        WAITING_FOR_NAME = 2
+        WAITING_FOR_MONEY_LIMIT = 3
+        WAITING_FOR_CURRENT_MONEY = 4
+        REGISTERING_CATHEGORY = 5
 
-class _ChooseCathegoryTypeHandler(BaseInnerHandler):
-    CHOOSE_TYPE_MESSAGE = ("Выберите тип категории:\n"
-                           "- Доходы\n"
-                           "- Расходы\n")
-
-    MARKUP = ReplyKeyboardMarkup()
-    EXPENSE_CATHEGORY_BUTTON_NAME = "Расходы"
-    INCOME_CATHEGORY_BUTTON_NAME = "Доходы"
-    CANCEL_CATHEGORY_BUTTON_NAME = "Отменить"
-    MARKUP.add(EXPENSE_CATHEGORY_BUTTON_NAME)
-    MARKUP.add(INCOME_CATHEGORY_BUTTON_NAME)
-    MARKUP.add(CANCEL_CATHEGORY_BUTTON_NAME)
-
-    def __init__(self, outter_handler: BaseHandler, cathegory: Cathegory) -> None:
+    def __init__(self, outter_handler: ReusableHandler, user_id: int) -> None:
         super().__init__(outter_handler)
-        self.cathegory = cathegory
+        self.new_cathegory = Cathegory()
+        self.new_cathegory.person_id = user_id
+        self.state = self.State.WAITING_FOR_TYPE
 
     def handle_message(self, message: Message):
-        if not message.text:
-            BotState().bot.send_message(message.chat.id, __class__.CHOOSE_TYPE_MESSAGE, reply_markup=__class__.MARKUP)    
-            return self
-        if message.text == __class__.EXPENSE_CATHEGORY_BUTTON_NAME:
-            self.cathegory.cathegory_type_id = DatabaseApi().get_expense_cathegory_type_id()
-            return _ChooseCathegoryNameHandler.switch_to_this_handler(message, self.outter_handler, self.cathegory)
-        elif message.text == __class__.INCOME_CATHEGORY_BUTTON_NAME:
-            self.cathegory.cathegory_type_id = DatabaseApi().get_income_cathegory_type_id()
-            return _ChooseCathegoryNameHandler.switch_to_this_handler(message, self.outter_handler, self.cathegory)
-        elif message.text == __class__.CANCEL_CATHEGORY_BUTTON_NAME:
-            return self.switch_to_existing_handler(message)
-        else:
-            BotState().bot.send_message(message.chat.id, __class__.CHOOSE_TYPE_MESSAGE, reply_markup=__class__.MARKUP)    
-            return self
-
-    @staticmethod
-    def switch_to_this_handler(message: Message, outter_handler, cathegory: Cathegory):
-        BotState().bot.send_message(message.chat.id, __class__.CHOOSE_TYPE_MESSAGE, reply_markup=__class__.MARKUP)
-        return __class__(outter_handler, cathegory)
-
-
-class _ChooseCathegoryNameHandler(BaseInnerHandler):
-    CHOOSE_NAME_MESSAGE = "Введите название категории"
-    NAME_TOO_SHORT_MESSAGE = "Имя должно содержать минимум один символ"
-
-    MARKUP = ReplyKeyboardRemove()
-
-    def __init__(self, outter_handler: BaseHandler, cathegory: Cathegory) -> None:
-        super().__init__(outter_handler)
-        self.cathegory = cathegory
-
-    def handle_message(self, message: Message):
-        if not message.text:
-            BotState().bot.send_message(__class__.CHOOSE_NAME_MESSAGE, reply_markup=__class__.MARKUP)    
-            return self
-        if len(message.text.strip()) < 1:
-            BotState().bot.send_message(__class__.NAME_TOO_SHORT_MESSAGE, reply_markup=__class__.MARKUP)
-            return self
-        self.cathegory.name = message.text
-        return _CathegoryMoneyLimitHandler.switch_to_this_handler(message, self.outter_handler, self.cathegory)
-
-
-    @staticmethod
-    def switch_to_this_handler(message: Message, outter_handler, cathegory: Cathegory):
-        BotState().bot.send_message(message.chat.id, __class__.CHOOSE_NAME_MESSAGE, reply_markup=__class__.MARKUP)
-        return __class__(outter_handler, cathegory)
-    
-class _CathegoryMoneyLimitHandler(BaseInnerHandler):
-    CHOOSE_LIMIT_MESSAGE = "Введите желаемое ограничение по сумме для данной категории"
-    BAD_LIMIT_MESSAGE = "Введено некорректное значение. Попробуйте снова"
-
-    MARKUP = ReplyKeyboardMarkup()
-    MARKUP.add("0", "1000")
-    MARKUP.add("5000", "10000")
-    MARKUP.add("20000", "30000")
-
-    def __init__(self, outter_handler: BaseHandler, cathegory: Cathegory) -> None:
-        super().__init__(outter_handler)
-        self.cathegory = cathegory
-
-    def handle_message(self, message: Message):
-        if not message.text:
-            BotState().bot.send_message(__class__.CHOOSE_LIMIT_MESSAGE, reply_markup=__class__.MARKUP)    
-            return self
-        try:
-            limit = int(message.text)
-            if limit < 0: raise ValueError()
-            self.cathegory.money_limit = limit
-            return _CathegoryCurrentMoneyHandler.switch_to_this_handler(message, self.outter_handler, self.cathegory)
-        except ValueError:
-            BotState().bot.send_message(__class__.BAD_LIMIT_MESSAGE, reply_markup=__class__.MARKUP)
-            return self
-
-    @staticmethod
-    def switch_to_this_handler(message: Message, outter_handler, cathegory: Cathegory):
-        BotState().bot.send_message(message.chat.id, __class__.CHOOSE_LIMIT_MESSAGE, reply_markup=__class__.MARKUP)
-        return __class__(outter_handler, cathegory)
-
-class _CathegoryCurrentMoneyHandler(BaseInnerHandler):
-    CHOOSE_EXPENSES_MESSAGE = "Введите текущие затраты по данной категории"
-    BAD_LIMIT_MESSAGE = "Введено некорректное значение. Попробуйте снова"
-
-    MARKUP = ReplyKeyboardMarkup()
-    MARKUP.add("0", "1000")
-    MARKUP.add("5000", "10000")
-    MARKUP.add("20000", "30000")
-
-    def __init__(self, outter_handler: BaseHandler, cathegory: Cathegory) -> None:
-        super().__init__(outter_handler)
-        self.cathegory = cathegory
-
-    def handle_message(self, message: Message):
-        if not message.text:
-            BotState().bot.send_message(__class__.CHOOSE_EXPENSES_MESSAGE, reply_markup=__class__.MARKUP)    
-            return self
-        try:
-            limit = int(message.text)
-            if limit < 0: raise ValueError()
-            self.cathegory.current_money = limit
-            return _RegisterCathegoryHandler.switch_to_this_handler(message, self.outter_handler, self.cathegory)
-        except ValueError:
-            BotState().bot.send_message(__class__.BAD_LIMIT_MESSAGE, reply_markup=__class__.MARKUP)
-            return self
-
-    @staticmethod
-    def switch_to_this_handler(message: Message, outter_handler, cathegory: Cathegory):
-        BotState().bot.send_message(message.chat.id, __class__.CHOOSE_EXPENSES_MESSAGE, reply_markup=__class__.MARKUP)
-        return __class__(outter_handler, cathegory)
-
-class _RegisterCathegoryHandler(BaseInnerHandler):
-    SUCCESS_MESSAGE = "Категория успешно создана!"
-
-    MARKUP = ReplyKeyboardRemove()
-
-    def __init__(self, outter_handler: ReusableHandler, cathegory: Cathegory) -> None:
-        super().__init__(outter_handler)
-        self.cathegory = cathegory
-
-    def handle_message(self, message: Message):
-        if not self.cathegory.id:
-            self.cathegory.id = DatabaseApi().add_cathegory(self.cathegory)
-            self.outter_handler.cathegories.append(self.cathegory)
+        assert self.state == __class__.State.REGISTERING_CATHEGORY
+        if not self.new_cathegory.id:
+            self.new_cathegory.id = DatabaseApi().add_cathegory(self.new_cathegory)
+        self.outter_handler.return_result = self.new_cathegory
+        BotState().bot.send_message(message.chat.id, "Категория успешно создана!")
         return self.outter_handler.switch_to_existing_handler(message)
 
     @staticmethod
-    def switch_to_this_handler(message: Message, outter_handler, cathegory: Cathegory):
-        try:
-            cathegory.id = DatabaseApi().add_cathegory(cathegory)
-            if cathegory.cathegory_type_id == DatabaseApi().get_expense_cathegory_type_id():
-                outter_handler.expense_cathegories.append(cathegory)
-            else:
-                outter_handler.income_cathegories.append(cathegory)
-            BotState().bot.send_message(message.chat.id, __class__.SUCCESS_MESSAGE, reply_markup=__class__.MARKUP)
-            return outter_handler.switch_to_existing_handler(message)
-        except Exception:
-            logging.error("Failed inserting cathegory in database")
-            return __class__(outter_handler, cathegory)
+    def switch_to_this_handler(message: Message, outter_handler) -> BaseHandler:
+        this_handler = __class__(outter_handler, message.from_user.id)
+        return this_handler.__call_choose_type_handler(message)
 
+    def __call_choose_type_handler(self, message) -> ChooseCathegoryTypeHandler:
+        return ChooseCathegoryTypeHandler.switch_to_this_handler(message, self)
+
+    class GetName:
+        ASKING_MESSAGE = "Введите название категории"
+        ERROR_MESSAGE = "Имя не может быть пустым"
+
+        @staticmethod
+        def is_valid_name(name: str) -> tuple[bool, str]:
+            if len(name) == 0:
+                return (False, __class__.ERROR_MESSAGE)
+            return (True, "")
+
+    def __call_enter_name_handler(self, message) -> GetTextHandler:
+        prev_state = self.state
+        try:
+            self.state = __class__.State.WAITING_FOR_NAME
+            return GetTextHandler.switch_to_this_handler(
+                message,
+                self,
+                __class__.GetName.ASKING_MESSAGE,
+                is_valid=__class__.GetName.is_valid_name,
+            )
+        except:
+            self.state = prev_state
+            raise
+    
+    class GetMoneyLimit:
+        ASKING_MESSAGE = "Введите желаемое ограничение по сумме для данной категории"
+        BAD_LIMIT_MESSAGE = "Введено некорректное значение. Попробуйте снова"
+
+        MARKUP = ReplyKeyboardMarkup()
+        MARKUP.add("0", "1000")
+        MARKUP.add("5000", "10000")
+        MARKUP.add("20000", "30000")
+
+        @staticmethod
+        def is_valid_money_limit(limit: str) -> tuple[bool, str]:
+            if limit <= 0:
+                return (False, __class__.BAD_LIMIT_MESSAGE)
+            return (True, "")
+
+    def __call_enter_money_limit_handler(self, message) -> GetNumberHandler:
+        prev_state = self.state
+        try:
+            self.state = __class__.State.WAITING_FOR_MONEY_LIMIT
+            return GetNumberHandler.switch_to_this_handler(
+                message,
+                self,
+                __class__.GetMoneyLimit.ASKING_MESSAGE,
+                __class__.GetMoneyLimit.MARKUP,
+                is_valid=__class__.GetMoneyLimit.is_valid_money_limit
+            )
+        except:
+            self.state = prev_state
+            raise
+
+    class GetCurrentMoney:
+        ASKING_MESSAGE = "Введите текущие затраты/доходы по данной категории"
+        BAD_LIMIT_MESSAGE = "Введено некорректное значение. Попробуйте снова"
+
+        MARKUP = ReplyKeyboardMarkup()
+        MARKUP.add("0", "1000")
+        MARKUP.add("5000", "10000")
+        MARKUP.add("20000", "30000")
+
+    def __call_enter_current_money_handler(self, message) -> GetNumberHandler:
+        prev_state = self.state
+        try:
+            self.state = __class__.State.WAITING_FOR_CURRENT_MONEY
+            return GetNumberHandler.switch_to_this_handler(
+                message,
+                self,
+                __class__.GetMoneyLimit.ASKING_MESSAGE,
+                __class__.GetMoneyLimit.MARKUP,
+            )
+        except:
+            self.state = prev_state
+            raise
+    
+    def __register_cathegory(self, message: Message) -> GetNumberHandler:
+        try:
+            self.new_cathegory.id = DatabaseApi().add_cathegory(self.new_cathegory)
+            self.outter_handler.return_result = self.new_cathegory
+            BotState().bot.send_message(message.chat.id, "Категория успешно создана!")
+            return self.outter_handler.switch_to_existing_handler(message)
+        except:
+            try:
+                BotState().bot.send_message(message.chat.id, "Внутренняя ошибка")
+            except:
+                pass
+            return self
+
+    def switch_to_existing_handler(self, message: Message) -> ReusableHandler:
+        if self.state == self.State.WAITING_FOR_TYPE:
+            return self.got_cathegory_type_sh(message)
+        elif self.state == self.State.WAITING_FOR_NAME:
+            return self.got_cathegory_name_sh(message)
+        elif self.state == self.State.WAITING_FOR_MONEY_LIMIT:
+            return self.got_money_limit_sh(message)
+        elif self.state == self.State.WAITING_FOR_CURRENT_MONEY:
+            return self.got_current_money_sh(message)
+        assert False, "Something went wronk i guess..."
+
+    def got_cathegory_type_sh(self, message) -> BaseInnerHandler:
+        if not self.return_result:
+            self.outter_handler.return_result = None
+            return self.outter_handler.switch_to_existing_handler(message)
+        self.new_cathegory.cathegory_type_id = self.return_result
+        return self.__call_enter_name_handler(message)
+    
+    def got_cathegory_name_sh(self, message) -> BaseInnerHandler:
+        if not self.return_result:
+            self.outter_handler.return_result = None
+            return self.outter_handler.switch_to_existing_handler(message)
+        self.new_cathegory.name = self.return_result
+        return self.__call_enter_money_limit_handler(message)
+    
+    def got_money_limit_sh(self, message) -> BaseInnerHandler:
+        if not self.return_result:
+            self.outter_handler.return_result = None
+            return self.outter_handler.switch_to_existing_handler(message)
+        self.new_cathegory.money_limit = self.return_result
+        return self.__call_enter_current_money_handler(message)
+    
+    def got_current_money_sh(self, message) -> BaseInnerHandler:
+        if not self.return_result:
+            self.outter_handler.return_result = None
+            return self.outter_handler.switch_to_existing_handler(message)
+        self.new_cathegory.current_money = self.return_result
+        return self.__register_cathegory(message)
