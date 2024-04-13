1bbbaa7430923fa42cfb312678bf1c440493cef5
Abraamyan <samvelcc25@outlook.com>
2024-04-13 23:20:40 +0300

    markup logic and cancel button

--- src/messageprocessing/handlers/commonhandlers/get_text_handler.py
+++ src/messageprocessing/handlers/commonhandlers/get_text_handler.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
-from ..base_inner_hndl import ReturningResultHandler, BaseHandler, ReusableHandler
-from telebot.types import Message, ReplyKeyboardMarkup
-from botstate import BotState
+from ..base_inner_handler import ReturningResultHandler, BaseInnerHandler, ReusableHandler
+from telebot.types import Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from messageprocessing.botstate import BotState
 
 
 class GetTextHandler(ReturningResultHandler):
@@ -9,13 +9,15 @@ class GetTextHandler(ReturningResultHandler):
     CANCEL_NAME = "Отмена"
 
     def __init__(self, outter_handler: ReusableHandler, asking_message: str, 
-                 markup = ReplyKeyboardMarkup(), is_valid = lambda x: (True, "")) -> None:
+                 markup = ReplyKeyboardMarkup(), is_valid = lambda x: (True, ""),
+                 add_cancel_button=True) -> None:
         super().__init__(outter_handler)
         self.asking_message = asking_message
+        self.add_cancel_button = add_cancel_button
         self.markup = markup
         self.is_valid = is_valid
 
-    def handle_message(self, message: Message) -> BaseHandler:
+    def handle_message(self, message: Message) -> BaseInnerHandler:
         if not message.text:
             return self
         if message.text == __class__.CANCEL_NAME:
@@ -32,8 +34,15 @@ class GetTextHandler(ReturningResultHandler):
     @staticmethod
     def switch_to_this_handler(message: Message, outter_handler: ReusableHandler, 
                                asking_message: str, markup = None, 
-                               is_valid = lambda x: (True, "")) -> GetTextHandler:
+                               is_valid = lambda x: (True, ""),
+                               add_cancel_button=True) -> GetTextHandler:
         if not markup:
-            markup = ReplyKeyboardMarkup()
+            if not add_cancel_button:
+                markup = ReplyKeyboardRemove()
+            else:
+                markup = ReplyKeyboardMarkup()
+                markup.add(__class__.CANCEL_NAME)            
+        elif add_cancel_button:
+            markup.add(__class__.CANCEL_NAME)
         BotState().bot.send_message(message.chat.id, asking_message, reply_markup=markup)
-        return GetTextHandler(outter_handler, asking_message, markup, is_valid)
+        return GetTextHandler(outter_handler, asking_message, markup, is_valid, add_cancel_button)
