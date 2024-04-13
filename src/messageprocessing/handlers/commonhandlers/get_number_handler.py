814d9d5e6dc6a6619de55bff2852c56009e056ae
Abraamyan <samvelcc25@outlook.com>
2024-04-13 23:20:56 +0300

    markup logic, cancel button, rename

--- src/messageprocessing/handlers/commonhandlers/get_number_handler.py
+++ src/messageprocessing/handlers/commonhandlers/get_number_handler.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
-from ..base_inner_hndl import ReturningResultHandler, BaseHandler, ReusableHandler
-from telebot.types import Message, ReplyKeyboardMarkup
-from botstate import BotState
+from ..base_inner_handler import ReturningResultHandler, BaseInnerHandler, ReusableHandler
+from telebot.types import Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from messageprocessing.botstate import BotState
 
 
 class GetNumberHandler(ReturningResultHandler):
@@ -10,21 +10,23 @@ class GetNumberHandler(ReturningResultHandler):
     CANCEL_NAME = "Отмена"
 
     def __init__(self, outter_handler: ReusableHandler, asking_message: str, 
-                 markup = ReplyKeyboardMarkup(), pred = lambda x: (True, "")) -> None:
+                 markup = None, is_valid = lambda x: (True, ""),
+                 add_cancel_button=True) -> None:
         super().__init__(outter_handler)
         self.asking_message = asking_message
+        self.is_valid = is_valid
+        self.add_cancel_button = add_cancel_button
         self.markup = markup
-        self.pred = pred
 
-    def handle_message(self, message: Message) -> BaseHandler:
+    def handle_message(self, message: Message) -> BaseInnerHandler:
         if not message.text:
             return self
-        if message.text == __class__.CANCEL_NAME:
+        if self.add_cancel_button and message.text == __class__.CANCEL_NAME:
             self.outter_handler.return_result = None
             return self.outter_handler.switch_to_existing_handler(message)
         try:
             value = int(message.text)
-            result, err = self.pred(value)
+            result, err = self.is_valid(value)
             if not result:
                 BotState().bot.send_message(message.chat.id, err, reply_markup=self.markup)
                 BotState().bot.send_message(message.chat.id, self.asking_message, reply_markup=self.markup)
@@ -37,8 +39,21 @@ class GetNumberHandler(ReturningResultHandler):
 
     @staticmethod
     def switch_to_this_handler(message: Message, outter_handler: ReusableHandler, 
-                               asking_message: str, markup = ReplyKeyboardMarkup(), 
-                               pred = lambda x: (True, "")) -> GetNumberHandler:
-        markup.add(__class__.CANCEL_NAME)
+                               asking_message: str, markup = None, 
+                               is_valid = lambda x: (True, ""),
+                               add_cancel_button=True) -> GetNumberHandler:
+        """
+            return_result:
+                - None if cencel choosed
+                - int Number that pred(Number) == True
+        """
+        if not markup:
+            if not add_cancel_button:
+                markup = ReplyKeyboardRemove()
+            else:
+                markup = ReplyKeyboardMarkup()
+                markup.add(__class__.CANCEL_NAME)            
+        elif add_cancel_button:
+            markup.add(__class__.CANCEL_NAME)
         BotState().bot.send_message(message.chat.id, asking_message, reply_markup=markup)
-        return GetNumberHandler(outter_handler, asking_message, markup, pred)
+        return GetNumberHandler(outter_handler, asking_message, markup, is_valid, add_cancel_button)
