97f51862a43a7bfa496acc7771951c1dfde4baab
Abraamyan Alexander <samvelcc25@outlook.com>
2024-04-20 23:35:43 +0300

    fix: tuple instead of str was returned

--- src/messageprocessing/handlers/operationshandler/utils.py
+++ src/messageprocessing/handlers/operationshandler/utils.py
@@ -9,7 +9,7 @@ def operation_to_str(operation: Operation) -> str:
         - Different database exceptions
     """
     cathegory_name = DatabaseApi().get_cathegory_by_id(operation.cathegory_id).name
-    operation_str = (f"Категория: {cathegory_name}\n",
-                     f"\tИзменение баланса: {operation.money_amount}\n",
+    operation_str = (f"Категория: {cathegory_name}\n"
+                     f"\tИзменение баланса: {operation.money_amount}\n"
                      f"\tКомментарий: {operation.comment[:50] + '...' if len(operation.comment) > 50 else operation.comment}")
     return operation_str
